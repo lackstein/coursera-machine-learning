4f79a3e6511b71ef88841ee50a2e0cdbc949c293
Noah Lackstein <noah@lackstein.com>
2015-02-07 15:56:07 -0500

    Plot data and compute cost function for one variable

--- Assignment 1/mlclass-ex1/computeCost.m
+++ Assignment 1/mlclass-ex1/computeCost.m
@@ -7,14 +7,15 @@ function J = computeCost(X, y, theta)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
+%J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
+h = X * theta;
+squared_errors = (h - y).^2;
+J = sum(squared_errors) / (2 * m);
 
 
 % =========================================================================
