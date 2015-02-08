5e9347d8eba3fddc875e90fd3ac799a0abd6c8c1
Noah Lackstein <noah@lackstein.com>
2015-02-07 20:41:36 -0500

    Multiple feature gradient descent

--- Assignment 1/mlclass-ex1/gradientDescentMulti.m
+++ Assignment 1/mlclass-ex1/gradientDescentMulti.m
@@ -16,14 +16,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
+    
+    h = X * theta;
+    coef = (X' * (h - y)) / m;
+    theta = theta - alpha * coef;
 
 
 
